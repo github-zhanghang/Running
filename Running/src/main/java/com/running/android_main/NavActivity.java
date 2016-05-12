18ddea272218716df19951fd018a2768841e7ece
ZhangHang <1083197619@qq.com>
2016-05-12 18:52:16 +0800

    run2

--- Running/src/main/java/com/running/android_main/NavActivity.java
+++ Running/src/main/java/com/running/android_main/NavActivity.java
@@ -21,9 +21,6 @@ import cn.smssdk.gui.RegisterPage;
 
 public class NavActivity extends AppCompatActivity implements View.OnClickListener {
     private String mPath = "http://192.168.191.1:8080/JavaWeb-0406_homework/forwardServlet";
-    //短信验证
-    private final static String APP_KEY = "12a1127a423ef";
-    private final static String APP_SECRET = "4d8275b912a9aceb7f39fdbf91b92da4";
     private EditText mNameEditText, mPasswordEditText;
     private Button mLoginButton, mRegisterButton;
     private ProgressDialog mProgressDialog;
@@ -54,12 +51,23 @@ public class NavActivity extends AppCompatActivity implements View.OnClickListen
         switch (v.getId()) {
             case R.id.login:
                 //处理登录
-                mProgressDialog = ProgressDialog.show(this, "请等待...", "正在为您登录...");
-                handleLogin(mNameEditText.getText().toString(), mPasswordEditText.getText().toString());
+                String uName = mNameEditText.getText().toString();
+                String uPwd = mPasswordEditText.getText().toString();
+                //仅用于测试时使用
+                if (uName.equals("zh") && uPwd.equals("123")) {
+                    Intent intent = new Intent(NavActivity.this, MainActivity.class);
+                    intent.putExtra("username", mNameEditText.getText().toString());
+                    startActivity(intent);
+                    NavActivity.this.finish();
+                } else {
+                    //真实登录代码
+                    mProgressDialog = ProgressDialog.show(this, "请等待...", "正在为您登录...");
+                    handleLogin(uName, uPwd);
+                }
                 break;
             case R.id.regist:
-                //处理注册
-                handleRegister();
+                //跳转注册页面
+                startActivity(new Intent(NavActivity.this, RegisterActivity.class));
                 break;
         }
     }
@@ -68,7 +76,6 @@ public class NavActivity extends AppCompatActivity implements View.OnClickListen
         RequestParams params = new RequestParams(mPath);
         params.addQueryStringParameter("username", username);
         params.addQueryStringParameter("password", password);
-        params.setConnectTimeout(5000);
         x.http().get(params, new Callback.CommonCallback<String>() {
             @Override
             public void onSuccess(String result) {
@@ -99,28 +106,5 @@ public class NavActivity extends AppCompatActivity implements View.OnClickListen
             public void onFinished() {
             }
         });
-    }
-
-    private void handleRegister() {
-        //初始化短信验证
-        SMSSDK.initSDK(this, APP_KEY, APP_SECRET);
-        RegisterPage registerPage = new RegisterPage();
-        registerPage.setRegisterCallback(new EventHandler() {
-            public void afterEvent(int event, int result, Object data) {
-                // 解析注册结果
-                if (result == SMSSDK.RESULT_COMPLETE) {
-                    @SuppressWarnings("unchecked")
-                    HashMap<String, Object> phoneMap = (HashMap<String, Object>) data;
-                    String country = (String) phoneMap.get("country");
-                    String phone = (String) phoneMap.get("phone");
-                    // 提交用户信息
-                    //参数uid/nickname昵称/avatar头像/country国家/phone电话
-                    SMSSDK.submitUserInfo(phone, "行长", null, country, phone);
-                } else {
-                    Toast.makeText(getApplicationContext(), "注册失败", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        registerPage.show(NavActivity.this);
     }
 }
