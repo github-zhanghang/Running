2afee14ec13be5337403114cf15bcae1669aef95
ZhangHang <1083197619@qq.com>
2016-05-14 19:46:01 +0800

    run2

---
+++ Running/src/main/java/com/running/android_main/RunMapActivity.java
@@ -0,0 +1,24 @@
+package com.running.android_main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
+
+public class RunMapActivity extends AppCompatActivity {
+    private MapView mMapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_runmap);
+        initViews();
+    }
+
+    private void initViews() {
+        mMapView = (MapView) findViewById(R.id.mapview);
+    }
+}
--- Running/src/main/java/com/running/fragments/PaobuLeftFragment.java
+++ Running/src/main/java/com/running/fragments/PaobuLeftFragment.java
@@ -1,5 +1,6 @@
 package com.running.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,17 +9,31 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.running.android_main.R;
+import com.running.android_main.RunMapActivity;
+import com.running.myviews.CircleButton;
 
 /**
  * Created by ZhangHang on 2016/5/5.
  */
 public class PaobuLeftFragment extends Fragment {
     private View mLeftView;
+    private CircleButton mStartButton;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mLeftView = inflater.inflate(R.layout.paobu_left, null);
+        mStartButton = (CircleButton) mLeftView.findViewById(R.id.startRun);
+        initListeners();
         return mLeftView;
+    }
+
+    private void initListeners() {
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), RunMapActivity.class));
+            }
+        });
     }
 }
