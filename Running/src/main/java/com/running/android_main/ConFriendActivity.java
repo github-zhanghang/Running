88906a77c4ba26e2a0e15805885fdb88169e65a0
EzioL <814711679@qq.com>
2016-06-12 20:12:07 +0800

    条件搜索

--- Running/src/main/java/com/running/android_main/ConFriendActivity.java
+++ Running/src/main/java/com/running/android_main/ConFriendActivity.java
@@ -3,13 +3,16 @@ package com.running.android_main;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.baidu.platform.comapi.map.C;
 import com.running.adapters.ConFriendAdapter;
 import com.running.beans.UserInfo;
+import com.running.myviews.TopBar;
 
 import java.util.List;
 
@@ -17,9 +20,12 @@ public class ConFriendActivity extends AppCompatActivity {
     private List<UserInfo> mUserInfoList;
     private ListView mListView;
     private ConFriendAdapter mConFriendAdapter;
+    private TopBar mTopBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_con_friend);
+
         initViews();
         initData();
         clickListener();
@@ -29,22 +35,35 @@ public class ConFriendActivity extends AppCompatActivity {
 
 
     private void initViews() {
+
         mUserInfoList = (List<UserInfo>) getIntent().getExtras().get("SearchConditions");
         mListView = (ListView) findViewById(R.id.confriend_lv);
+        mTopBar = (TopBar) findViewById(R.id.confriend_TopBar);
+        Log.e("Ezio123", "initViews: "+mUserInfoList.size());
+
+    }
+    private void initData() {
         mConFriendAdapter = new ConFriendAdapter(ConFriendActivity.this,mUserInfoList);
         mListView.setAdapter(mConFriendAdapter);
     }
-    private void initData() {
-
-    }
     private void clickListener() {
+        mTopBar.setOnTopbarClickListener(new TopBar.OnTopbarClickListener() {
+            @Override
+            public void onTopbarLeftImageClick(ImageView imageView) {
+                ConFriendActivity.this.finish();
+            }
+
+            @Override
+            public void onTopbarRightImageClick(ImageView imageView) {
+            }
+        });
+
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(ConFriendActivity.this, NewFriendInfoActivity.class);
                 intent.putExtra("FriendInfo", mUserInfoList.get(position));
                 startActivity(intent);
-
             }
         });
     }
