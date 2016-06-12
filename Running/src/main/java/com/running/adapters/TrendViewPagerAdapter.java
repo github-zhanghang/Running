d918d6ae4c04180b38dc5f02f370ee9ebd663582
ZhangHang <1083197619@qq.com>
2016-06-12 22:16:41 +0800

    history

--- Running/src/main/java/com/running/adapters/TrendViewPagerAdapter.java
+++ Running/src/main/java/com/running/adapters/TrendViewPagerAdapter.java
@@ -1,27 +1,12 @@
 package com.running.adapters;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.support.v4.view.PagerAdapter;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 
-import com.running.android_main.R;
-import com.running.beans.TrendData;
-
-import org.achartengine.ChartFactory;
 import org.achartengine.GraphicalView;
-import org.achartengine.chart.BarChart;
-import org.achartengine.model.XYMultipleSeriesDataset;
-import org.achartengine.model.XYSeries;
-import org.achartengine.renderer.SimpleSeriesRenderer;
-import org.achartengine.renderer.XYMultipleSeriesRenderer;
-import org.achartengine.renderer.XYSeriesRenderer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,125 +15,36 @@ import java.util.List;
  * Created by C5-0 on 2016/5/28.
  */
 public class TrendViewPagerAdapter extends PagerAdapter {
-    List<TrendData> trendDataList=new ArrayList<>();
-    List<LinearLayout> linearLayouts=new ArrayList<>();
+    List<GraphicalView> mListGraphicalView = new ArrayList<>();
     Context mContext;
     LayoutInflater mInflater;
 
-    public TrendViewPagerAdapter(Context mContext,List<TrendData> list) {
+    public TrendViewPagerAdapter(Context mContext, List<GraphicalView> listGraphicalView) {
         this.mContext = mContext;
-        this.trendDataList=list;
-        mInflater.from(mContext).inflate(R.layout.activity_trend,null);
-
-       /* for (int i = 0; i <5 ; i++) {
-            trendDataList.add(new TrendData());
-        }*/
-
-        for (int i = 0; i <trendDataList.size() ; i++) {
-            LinearLayout linearLayout=new LinearLayout(mContext);
-            linearLayout.addView(xychar(trendDataList.get(i).getValue()), new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-            linearLayouts.add(linearLayout);
-        }
-
+        this.mListGraphicalView = listGraphicalView;
     }
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-
-       // Log.e("taozi linearlayout",linearLayouts.size()+"" );
-
-
-
-        container.addView(linearLayouts.get(position));
-        return linearLayouts.get(position);
+        container.addView(mListGraphicalView.get(position));
+        return mListGraphicalView.get(position);
 
     }
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
         //销毁缓存
-        container.removeView(linearLayouts.get(position));
+        container.removeView(mListGraphicalView.get(position));
     }
 
     @Override
     public int getCount() {
-        return trendDataList.size();
+        return mListGraphicalView.size();
     }
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
-        return view==object;
+        return view == object;
     }
 
-
-
-    //柱状图
-    public GraphicalView xychar(double []yLable) {
-
-        //存放柱状图的值
-        int [] xLable=new int[]{0,1,2,3,4,5,6,7};
-        //设置柱状图的范围
-        double[] range=new double[]{0.5,7.5, 0, 10};
-
-        //渲染,创建你需要的图表最下面的图层
-        XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
-        //创建你需要在图层上显示的具体内容的图层
-        XYSeriesRenderer xyRenderer = new XYSeriesRenderer();
-        // 将要绘制的点添加到坐标绘制中
-        renderer.addSeriesRenderer(xyRenderer);
-        //创建数据层
-        XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
-        //创建具体的数据层
-        XYSeries series = new XYSeries("");
-
-        for (int j=0;j<yLable.length;j++) {
-            series.add(xLable[j+1],yLable[j]);
-        }
-        dataset.addSeries(series);
-        // 设置颜色
-        int color= Color.argb(255,255,153,0);
-        xyRenderer.setColor(color);
-        // 设置 X 轴不显示数字（改用我们手动添加的文字标签）
-        renderer.setShowLegend(false);
-        renderer.setXLabels(0);
-        //设置x轴标签数
-        renderer.setXLabels(xLable.length);
-        //设置x轴和y轴的标签对齐方式
-        renderer.setXLabelsAlign(Paint.Align.RIGHT);
-        renderer.setYLabelsAlign(Paint.Align.RIGHT);
-        // 设置现实网格
-        renderer.setShowGrid(false);
-        //设置是否需要显示坐标轴
-        renderer.setShowAxes(true);
-        // 设置轴标签文本大小
-        renderer.setLabelsTextSize(25);
-        // 设置条形图之间的距离
-        renderer.setBarSpacing(1.0);
-        // 调整合适的位置
-        renderer.setFitLegend(true);
-        //表盘移动
-        renderer.setPanEnabled(false,false );
-        //不可缩放
-        renderer.setZoomEnabled(false, false);
-        renderer.setClickEnabled(false);
-        renderer.setZoomButtonsVisible(false);
-        //设置图例的字体大小
-        renderer.setLegendTextSize(0);
-        //设置x轴和y轴的最大最小值
-        renderer.setRange(range);
-        //设置标签倾斜度
-        renderer.setXLabelsAngle(-45f);
-        renderer.setMarginsColor(0x00888888);
-
-        for (int i = 0; i < renderer.getSeriesRendererCount(); i++) {
-            SimpleSeriesRenderer ssr = renderer.getSeriesRendererAt(i);
-            ssr.setChartValuesTextAlign(Paint.Align.RIGHT);
-            ssr.setChartValuesTextSize(30);
-            ssr.setDisplayChartValues(true);// 在柱子顶端显示数值
-        }
-
-        GraphicalView mChartView = ChartFactory.getBarChartView(mContext,
-                dataset, renderer, BarChart.Type.DEFAULT);
-        return mChartView;
-    }
 }
--- Running/src/main/java/com/running/android_main/TrendActivity.java
+++ Running/src/main/java/com/running/android_main/TrendActivity.java
@@ -1,214 +1,223 @@
 package com.running.android_main;
 
-
-
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
-import com.google.gson.Gson;
 import com.running.adapters.TrendViewPagerAdapter;
-import com.running.beans.History;
 import com.running.beans.TrendData;
-import com.zhy.http.okhttp.OkHttpUtils;
-import com.zhy.http.okhttp.callback.StringCallback;
+import com.running.utils.TimeUtil;
+import com.yolanda.nohttp.NoHttp;
+import com.yolanda.nohttp.OnResponseListener;
+import com.yolanda.nohttp.Request;
+import com.yolanda.nohttp.RequestMethod;
+import com.yolanda.nohttp.RequestQueue;
+import com.yolanda.nohttp.Response;
 
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.BarChart;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.SimpleSeriesRenderer;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
-
-import okhttp3.Call;
 
 public class TrendActivity extends AppCompatActivity {
+    private final String mPath = MyApplication.HOST + "trendServlet";
     private MyApplication myApplication;
-    int uid;
-    List<TrendData> mTrendDataList=new ArrayList<>();
-    TextView mondayTextView,sundayTextView,walkTextView,distanceTextView,timeTextView,carlorieTextView;
-    ViewPager mViewPager;
-    TrendViewPagerAdapter mViewPagerAdapter;
-   /* int cachePagers = 1; //ViewPager缓存页面数目;当前页面的相邻N各页面都会被缓存
-    SimpleDateFormat sf = new SimpleDateFormat("EEE MMM dd hh:mm:ss z yyyy", Locale.ENGLISH);*/
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-    private String dateString;
-    Date date=new Date();
+    private ViewPager mViewPager;
+    private TrendViewPagerAdapter mViewPagerAdapter;
+    private int uid;
+    private List<TrendData> mTrendDataList = new ArrayList<>();
+    private TextView mondayTextView, sundayTextView, walkTextView, distanceTextView, timeTextView, carlorieTextView;
 
-    // 取得当前日期所在周的第一天
-    public static Date getFirstDayOfWeek(Date date) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setFirstDayOfWeek(Calendar.MONDAY);
-        calendar.setTime(date);
-        calendar.set(Calendar.DAY_OF_WEEK, calendar.getFirstDayOfWeek()); // Sunday
-        return calendar.getTime();
-    }
-
-    // 取得当前日期所在周的最后一天
-    public static Date getLastDayOfWeek(Date date) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setFirstDayOfWeek(Calendar.MONDAY);
-        calendar.setTime(date);
-        calendar.set(Calendar.DAY_OF_WEEK, calendar.getFirstDayOfWeek() + 6); // Saturday
-        return calendar.getTime();
-    }
-    //判断某天为周几
-    public static int getWeekDay(Date date){
-        Calendar cal=Calendar.getInstance();
-        cal.setTime(date);
-        int week=cal.get(Calendar.DAY_OF_WEEK)-1;
-        return week;
-    }
+    private RequestQueue mRequestQueue = NoHttp.newRequestQueue(1);
+    private int mPage;
+    //一周的数据
+    private List<Double> datas = new ArrayList<>();
+    private List<GraphicalView> mList = new ArrayList<>();
+    //一周的开始时间
+    private long startTime = TimeUtil.getWeekBegin(0);
+    //七天的毫秒
+    private long sevenDays = 7 * 24 * 60 * 60 * 1000;
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trend);
-
         myApplication = (MyApplication) getApplication();
-        uid=myApplication.getUserInfo().getUid();
-
+        uid = myApplication.getUserInfo().getUid();
         initView();
-        refreshData(0);
-        initListener();
-
-        //mViewPager.setPageMargin(getResources().getDimensionPixelOffset(R.dimen.trend_magin));
-        //mViewPager.setOffscreenPageLimit(cachePagers);// 设置缓存页面，当前页面的相邻N各页面都会被缓存
+        addListener();
+        //获取一共有多少周
+        getTotalWeek();
     }
 
     private void initView() {
-        mViewPager= (ViewPager) findViewById(R.id.vp_trend);
-        mondayTextView= (TextView) findViewById(R.id.monday_trend);
-        sundayTextView= (TextView) findViewById(R.id.sunday_trend);
-        walkTextView= (TextView) findViewById(R.id.walk_trend);
-        distanceTextView= (TextView) findViewById(R.id.distance_trend);
-        timeTextView= (TextView) findViewById(R.id.time_trend);
-        carlorieTextView= (TextView) findViewById(R.id.calorie_trend);
+        mViewPager = (ViewPager) findViewById(R.id.vp_trend);
+        mondayTextView = (TextView) findViewById(R.id.monday_trend);
+        sundayTextView = (TextView) findViewById(R.id.sunday_trend);
+        sundayTextView.setText(sdf.format(startTime));
+        mondayTextView.setText(sdf.format(startTime + sevenDays));
+        walkTextView = (TextView) findViewById(R.id.walk_trend);
+        distanceTextView = (TextView) findViewById(R.id.distance_trend);
+        timeTextView = (TextView) findViewById(R.id.time_trend);
+        carlorieTextView = (TextView) findViewById(R.id.calorie_trend);
     }
 
-
-    private void refreshData(final int position) {
-        date=new Date(date.getTime()-position*7*24*60*60*1000);
-        dateString= String.valueOf(date.getTime());
-
-        OkHttpUtils.get()
-                .url(MyApplication.HOST+"trendServlet")
-                .addParams("date",dateString)
-                .addParams("uid",uid+"")
-                .build()
-                .execute(new StringCallback() {
-                    @Override
-                    public void onError(Call call, Exception e) {
-                        Log.e("taozi", "refresh--onError: "+e.getMessage());
-                    }
-
-                    @Override
-                    public void onResponse(String response) {
-                        Log.e("taozi", "refresh--onResponse: "+response);
-                        try {
-                            JSONArray jsonArray = new JSONArray(response);
-                            TrendData trendData=new TrendData();
-                            for (int i = 0; i <jsonArray.length() ; i++) {
-                                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                History history=new Gson().fromJson(jsonObject.toString(),History.class);
-
-                                String monday=sdf.format(getFirstDayOfWeek(date));
-                               // Log.e("taozimonday"," refresh:"+monday );
-                                String sunday=sdf.format(getLastDayOfWeek(date));
-                                trendData.setMonday(monday);
-                                trendData.setSunday(sunday);
-                                trendData.setWalkStep(trendData.getWalkStep()+history.getStepcount());
-                                trendData.setDistance(trendData.getDistance()+history.getRundistance());
-                                trendData.setTime(trendData.getTime()+history.getRuntime());
-                                trendData.setCalorie(trendData.getCalorie()+history.getCalories());
-
-                                //把每天的跑步距离存储到相应的日期中
-                                if (getWeekDay(new Date(history.getRunstarttime()))==1){
-                                    trendData.getValue()[0]+=history.getRundistance();
-                                }
-                                if (getWeekDay(new Date(history.getRunstarttime()))==2){
-                                    trendData.getValue()[1]+=history.getRundistance();
-                                }
-                                if (getWeekDay(new Date(history.getRunstarttime()))==3){
-                                    trendData.getValue()[2]+=history.getRundistance();
-                                }
-                                if (getWeekDay(new Date(history.getRunstarttime()))==4){
-                                    trendData.getValue()[3]+=history.getRundistance();
-                                }
-                                if (getWeekDay(new Date(history.getRunstarttime()))==5){
-                                    trendData.getValue()[4]+=history.getRundistance();
-                                }
-                                if (getWeekDay(new Date(history.getRunstarttime()))==6){
-                                    trendData.getValue()[5]+=history.getRundistance();
-                                }
-                                if (getWeekDay(new Date(history.getRunstarttime()))==7){
-                                    trendData.getValue()[6]+=history.getRundistance();
-                                }
-                            }
-                            mTrendDataList.add(trendData);
-
-                            mondayTextView.setText(mTrendDataList.get(position).getMonday());
-                            sundayTextView.setText(mTrendDataList.get(position).getSunday());
-                            walkTextView.setText((mTrendDataList.get(position).getWalkStep())/7+"");
-                            distanceTextView.setText(mTrendDataList.get(position).getDistance()+"");
-                            timeTextView.setText(mTrendDataList.get(position).getTime()/(1000*60*7)+"");
-                            carlorieTextView.setText(mTrendDataList.get(position).getCalorie()+"");
-                         /*   mondayTextView.setText(trendData.getMonday());
-                            sundayTextView.setText(trendData.getSunday());
-                            walkTextView.setText((trendData.getWalkStep())/7+"");
-                            distanceTextView.setText(trendData.getDistance()+"");
-                            timeTextView.setText(trendData.getTime()/(1000*60*7)+"");
-                            carlorieTextView.setText(trendData.getCalorie()+"");*/
-                            //设置适配器
-                           // Log.e( "taozisize","zong"+ mTrendDataList.size()+"");
-                            mViewPagerAdapter=new TrendViewPagerAdapter(TrendActivity.this,mTrendDataList);
-                            mViewPager.setAdapter(mViewPagerAdapter);
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                });
-
+    private void getTotalWeek() {
+        Request<String> request = NoHttp.createStringRequest(mPath, RequestMethod.POST);
+        request.add("type", "totalweek");
+        request.add("uid", myApplication.getUserInfo().getUid());
+        mRequestQueue.add(1, request, mOnResponseListener);
+        mRequestQueue.start();
     }
 
-   //设置滑动监听事件
-    private void initListener() {
+    private void initAdapter() {
+        for (int i = 0; i < mPage; i++) {
+            mList.add(xychar(listToDouble(datas)));
+        }
+        mViewPagerAdapter = new TrendViewPagerAdapter(TrendActivity.this, mList);
+        mViewPager.setAdapter(mViewPagerAdapter);
+        mViewPager.setCurrentItem(mPage - 1);
+    }
+
+    private OnResponseListener<String> mOnResponseListener = new OnResponseListener<String>() {
+        @Override
+        public void onStart(int what) {
+        }
+
+        @Override
+        public void onSucceed(int what, Response<String> response) {
+            String result = response.get();
+            Log.e("my", "trend.result=" + result);
+            if (what == 1) {
+                mPage = Integer.parseInt(result);
+                datas.add(1.0);
+                datas.add(2.0);
+                datas.add(3.0);
+                datas.add(4.0);
+                datas.add(5.0);
+                datas.add(6.0);
+                datas.add(7.0);
+                initAdapter();
+            }
+        }
+
+        @Override
+        public void onFailed(int what, String url, Object tag, Exception exception, int responseCode, long networkMillis) {
+            Toast.makeText(TrendActivity.this, "获取数据失败", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onFinish(int what) {
+        }
+    };
+
+    private void addListener() {
         mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
             }
 
             @Override
             public void onPageSelected(int position) {
-
-              refreshData(position);
-
+                Log.e("my", "position=" + position);
+                sundayTextView.setText(sdf.format(startTime - (mPage - position - 1) * sevenDays));
+                mondayTextView.setText(sdf.format(startTime - (mPage - position) * sevenDays));
             }
-
 
             @Override
             public void onPageScrollStateChanged(int state) {
-                //页面状态改变时调用
             }
         });
     }
 
+    public static double[] listToDouble(List<Double> list) {
+        double[] doubleData = new double[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            doubleData[i] = list.get(i);
+        }
+        return doubleData;
+    }
 
+    //柱状图
+    public GraphicalView xychar(double[] yLable) {
+        //存放柱状图的值
+        int[] xLable = new int[]{0, 1, 2, 3, 4, 5, 6, 7};
+        //设置柱状图的范围
+        double[] range = new double[]{0.5, 7.5, 0, 10};
 
+        //渲染,创建你需要的图表最下面的图层
+        XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
+        //创建你需要在图层上显示的具体内容的图层
+        XYSeriesRenderer xyRenderer = new XYSeriesRenderer();
+        // 将要绘制的点添加到坐标绘制中
+        renderer.addSeriesRenderer(xyRenderer);
+        //创建数据层
+        XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+        //创建具体的数据层
+        XYSeries series = new XYSeries("");
+
+        for (int j = 0; j < yLable.length; j++) {
+            series.add(xLable[j + 1], yLable[j]);
+        }
+        dataset.addSeries(series);
+        // 设置颜色
+        int color = Color.argb(255, 255, 153, 0);
+        xyRenderer.setColor(color);
+        // 设置 X 轴不显示数字（改用我们手动添加的文字标签）
+        renderer.setShowLegend(false);
+        renderer.setXLabels(0);
+        //设置x轴标签数
+        renderer.setXLabels(xLable.length);
+        //设置x轴和y轴的标签对齐方式
+        renderer.setXLabelsAlign(Paint.Align.RIGHT);
+        renderer.setYLabelsAlign(Paint.Align.RIGHT);
+        // 设置现实网格
+        renderer.setShowGrid(false);
+        //设置是否需要显示坐标轴
+        renderer.setShowAxes(true);
+        // 设置轴标签文本大小
+        renderer.setLabelsTextSize(25);
+        // 设置条形图之间的距离
+        renderer.setBarSpacing(1.0);
+        // 调整合适的位置
+        renderer.setFitLegend(true);
+        //表盘移动
+        renderer.setPanEnabled(false, false);
+        //不可缩放
+        renderer.setZoomEnabled(false, false);
+        renderer.setClickEnabled(false);
+        renderer.setZoomButtonsVisible(false);
+        //设置图例的字体大小
+        renderer.setLegendTextSize(0);
+        //设置x轴和y轴的最大最小值
+        renderer.setRange(range);
+        //设置标签倾斜度
+        renderer.setXLabelsAngle(-45f);
+        renderer.setMarginsColor(0x00888888);
+
+        for (int i = 0; i < renderer.getSeriesRendererCount(); i++) {
+            SimpleSeriesRenderer ssr = renderer.getSeriesRendererAt(i);
+            ssr.setChartValuesTextAlign(Paint.Align.RIGHT);
+            ssr.setChartValuesTextSize(30);
+            ssr.setDisplayChartValues(true);// 在柱子顶端显示数值
+        }
+
+        GraphicalView mChartView = ChartFactory.getBarChartView(TrendActivity.this,
+                dataset, renderer, BarChart.Type.DEFAULT);
+        return mChartView;
+    }
 }
