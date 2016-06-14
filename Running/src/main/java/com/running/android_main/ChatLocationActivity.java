fed6d61a740da20ae8df85d6c229ade4eda76f81
EzioL <814711679@qq.com>
2016-06-14 14:38:31 +0800

    位置消息1

---
+++ Running/src/main/java/com/running/android_main/ChatLocationActivity.java
@@ -0,0 +1,23 @@
+package com.running.android_main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_location);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
