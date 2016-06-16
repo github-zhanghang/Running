535a5021aa1dae64c6bbe82fa477570d6f9be72a
EastTudord <2022439918@qq.com>
2016-06-16 09:10:07 +0800

    调整

--- Running/src/main/java/com/running/adapters/DynamicOneselfAdapter.java
+++ Running/src/main/java/com/running/adapters/DynamicOneselfAdapter.java
@@ -2,7 +2,6 @@ package com.running.adapters;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.widget.RecyclerView;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.AbsoluteSizeSpan;
@@ -10,23 +9,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.google.gson.Gson;
 import com.running.android_main.DynamicCommentActivity;
 import com.running.android_main.DynamicOneselfActivity;
 import com.running.android_main.MyApplication;
-import com.running.android_main.PersonInformationActivity;
 import com.running.android_main.R;
 import com.running.beans.DynamicImgBean;
 import com.running.beans.DynamicOneselfBean;
-import com.running.beans.UserInfo;
 import com.running.myviews.ninegridview.ImageInfo;
 import com.running.myviews.ninegridview.NineGridView;
 import com.running.myviews.ninegridview.preview.ClickNineGridViewAdapter;
-import com.running.utils.GlideCircleTransform;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
 
@@ -44,22 +40,12 @@ import okhttp3.Call;
  * Created by ldd on 2016/5/30.
  * 个人动态适配器
  */
-public class DynamicOneselfAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class DynamicOneselfAdapter extends BaseAdapter {
     Context mContext;
     List<HashMap<String, Object>> mList;
     LayoutInflater mInflater;
 
-    private static final int IS_HEADER = -1;
-    private static final int IS_NORMAL = 0;
-    private static final int IS_FOOTER = 1;
-
     private String url = MyApplication.HOST + "dynamicOperateServlet";
-
-    private OnItemClickListener mListener;
-
-    public void setOnItemClickListener(OnItemClickListener listener) {
-        mListener = listener;
-    }
 
     public DynamicOneselfAdapter(Context context, List<HashMap<String, Object>> list) {
         mContext = context;
@@ -67,152 +53,13 @@ public class DynamicOneselfAdapter extends RecyclerView.Adapter<RecyclerView.Vie
         mInflater = LayoutInflater.from(mContext);
     }
 
-
-    @Override
-    public int getItemViewType(int position) {
-        if (position + 1 == getItemCount()) {
-            return IS_FOOTER;
-        } else if (position == 0) {
-            return IS_HEADER;
-        } else {
-            return IS_NORMAL;
-        }
-    }
-
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        if (viewType == IS_HEADER) {
-            View mHeadView = mInflater.inflate(R.layout.dynamic_header, parent, false);
-            return new HeaderViewHolder(mHeadView);
-        } else if (viewType == IS_FOOTER) {
-            View mFootView = mInflater.inflate(R.layout.dynamic_load_foot, parent, false);
-            return new FooterViewHolder(mFootView);
-        } else {
-            View view = mInflater.inflate(R.layout.dynamic_oneself_img_item, parent, false);
-            return new ViewHolder(view);
-        }
-    }
-
-    @Override
-    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
-        if (holder instanceof HeaderViewHolder) {
-            final UserInfo userInfo = (UserInfo) mList.get(position).get
-                    ("header");
-            ((HeaderViewHolder) holder).mBackImageView.setImageResource(R.drawable.bg_dynamic);
-            Glide.with(mContext)
-                    .load(userInfo.getImageUrl())
-                    .transform(new GlideCircleTransform(mContext))
-                    .into(((HeaderViewHolder) holder).mHeadImageView);
-            ((HeaderViewHolder) holder).mHeadImageView.setOnClickListener(
-                    new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Intent intent = new Intent();
-                            intent.putExtra("UserInfo", userInfo);
-                            intent.setClass(mContext, PersonInformationActivity.class);
-                            mContext.startActivity(intent);
-                        }
-                    });
-            ((HeaderViewHolder) holder).mNameTextView.setText(userInfo.getNickName());
-            if (userInfo.getSex().equals("男")) {
-                ((HeaderViewHolder) holder).mSexImageView.setImageResource(R.drawable.ic_sex_man);
-            } else if (userInfo.getSex().equals("女")) {
-                ((HeaderViewHolder) holder).mSexImageView.setImageResource(R.drawable.ic_sex_woman);
-            }
-        } else if (holder instanceof FooterViewHolder) {
-            ((FooterViewHolder) holder).mLoadText.setText("加载中");
-        } else if (holder instanceof ViewHolder) {
-            DynamicOneselfBean bean = (DynamicOneselfBean) mList.get(position).get
-                    ("DynamicOneselfBean");
-            SpannableStringBuilder time = formatDateTime(bean.getTime());
-            ((ViewHolder) holder).mTime.setText(time);
-            ((ViewHolder) holder).mContent.setText(bean.getContent());
-            List<ImageInfo> infoList = new ArrayList<>();
-            for (int i = 0; i < bean.getImgList().size(); i++) {
-                ImageInfo imageInfo = new ImageInfo();
-                imageInfo.setThumbnailUrl(bean.getImgList().get(i));
-                imageInfo.setBigImageUrl(bean.getImgList().get(i));
-                infoList.add(imageInfo);
-            }
-            ClickNineGridViewAdapter adapter = new ClickNineGridViewAdapter(mContext, infoList);
-            ((ViewHolder) holder).mGridView.setAdapter(adapter);
-            ((ViewHolder) holder).mPraiseCount.setText(String.valueOf(bean.getPraiseCount()));
-            ((ViewHolder) holder).mCommentCount.setText(String.valueOf(bean.getCommentCount()));
-
-            if (((DynamicOneselfBean) mList.get(position).get("DynamicOneselfBean"))
-                    .getPraiseStatus() == 1) {
-                ((ViewHolder) holder).mPraiseImg.setImageResource(R.drawable.praise_red);
-            }
-            ((ViewHolder) holder).mPraiseImg.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (((DynamicOneselfBean) mList.get(position).get("DynamicOneselfBean"))
-                            .getPraiseStatus() == 0) {
-                        ((DynamicOneselfBean) mList.get(position).get
-                                ("DynamicOneselfBean")).setPraiseStatus(1);
-                        ((DynamicOneselfBean) mList.get(position).get
-                                ("DynamicOneselfBean")).setPraiseCount(((DynamicOneselfBean)
-                                mList.get(position).get
-                                        ("DynamicOneselfBean")).getPraiseCount() + 1);
-                        ((ViewHolder) holder).mPraiseCount.setText(String.valueOf((
-                                (DynamicOneselfBean) mList.get(position).get
-                                        ("DynamicOneselfBean"))
-                                .getPraiseCount()));
-                        ((ViewHolder) holder).mPraiseImg.setImageResource(R.drawable
-                                .praise_red);
-                        addPraise(((DynamicOneselfBean) mList.get(position).get
-                                ("DynamicOneselfBean")).getdId());
-                    }
-                }
-            });
-
-            ((ViewHolder) holder).mPraiseCount.setText(""+((DynamicOneselfBean) mList.get
-                    (position).get
-                    ("DynamicOneselfBean")).getPraiseCount());
-
-            ((ViewHolder) holder).mCommentImg.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(mContext, DynamicCommentActivity.class);
-                    intent.putExtra("myId", (((MyApplication)((DynamicOneselfActivity)mContext)
-                            .getApplication
-                            ()).getUserInfo().getUid()));
-                    DynamicOneselfBean oneselfBean = (DynamicOneselfBean) mList.get(position).get
-                            ("DynamicOneselfBean");
-                    DynamicImgBean imgBean = new DynamicImgBean(oneselfBean.getdId(), oneselfBean
-                            .getName(), oneselfBean.getHeadPhoto(), oneselfBean.getImgList(),
-                            oneselfBean.getContent(), oneselfBean.getTime(), oneselfBean
-                            .getLocation(), oneselfBean.getPraiseCount(), oneselfBean
-                            .getCommentCount(), oneselfBean.getuId(), oneselfBean.getPraiseStatus());
-                    Log.d("TAG",imgBean.toString());
-                    intent.putExtra("dynamicBean", imgBean);
-                    mContext.startActivity(intent);
-                }
-            });
-
-            ((ViewHolder) holder).mCommentCount.setText(""+((DynamicOneselfBean)
-                    mList.get(position)
-                            .get("DynamicOneselfBean")).getCommentCount());
-
-            if (mListener == null) {
-                return;
-            }
-
-            /*holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    mListener.onItemClick(position, map);
-                }
-            });*/
-        }
-    }
-
     private void addPraise(int i) {
         HashMap<String, Integer> map = new HashMap<>();
         map.put("pDId", i);
         //用户id
-        //map.put("pUId",mMyApplication.getUserInfo().getUid());
-        map.put("pUId", 1);
+        map.put("pUId",((MyApplication)(((DynamicOneselfActivity)mContext).getApplication()))
+                .getUserInfo()
+                .getUid());
         Gson gson = new Gson();
         String praiseMap = gson.toJson(map);
         OkHttpUtils.post()
@@ -234,18 +81,109 @@ public class DynamicOneselfAdapter extends RecyclerView.Adapter<RecyclerView.Vie
     }
 
     @Override
-    public int getItemCount() {
+    public int getCount() {
         return mList.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    @Override
+    public Object getItem(int position) {
+        return mList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        final ViewHolder holder;
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.dynamic_oneself_img_item, parent, false);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        DynamicOneselfBean bean = (DynamicOneselfBean) mList.get(position).get
+                ("DynamicOneselfBean");
+        Log.e("LDD",bean.toString());
+        SpannableStringBuilder time = formatDateTime(bean.getTime());
+        holder.mTime.setText(time);
+        holder.mContent.setText(bean.getContent());
+        List<ImageInfo> infoList = new ArrayList<>();
+        for (int i = 0; i < bean.getImgList().size(); i++) {
+            ImageInfo imageInfo = new ImageInfo();
+            imageInfo.setThumbnailUrl(bean.getImgList().get(i));
+            imageInfo.setBigImageUrl(bean.getImgList().get(i));
+            infoList.add(imageInfo);
+        }
+        ClickNineGridViewAdapter adapter = new ClickNineGridViewAdapter(mContext, infoList);
+        holder.mGridView.setAdapter(adapter);
+        if (((DynamicOneselfBean) mList.get(position).get("DynamicOneselfBean"))
+                .getPraiseStatus() == 1) {
+            holder.mPraiseImg.setImageResource(R.drawable.praise_red);
+        }
+        holder.mPraiseImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (((DynamicOneselfBean) mList.get(position).get("DynamicOneselfBean"))
+                        .getPraiseStatus() == 0) {
+                    ((DynamicOneselfBean) mList.get(position).get
+                            ("DynamicOneselfBean")).setPraiseStatus(1);
+                    ((DynamicOneselfBean) mList.get(position).get
+                            ("DynamicOneselfBean")).setPraiseCount(((DynamicOneselfBean)
+                            mList.get(position).get
+                                    ("DynamicOneselfBean")).getPraiseCount() + 1);
+                    holder.mPraiseCount.setText(String.valueOf((
+                            (DynamicOneselfBean) mList.get(position).get
+                                    ("DynamicOneselfBean"))
+                            .getPraiseCount()));
+                    holder.mPraiseImg.setImageResource(R.drawable
+                            .praise_red);
+                    addPraise(((DynamicOneselfBean) mList.get(position).get
+                            ("DynamicOneselfBean")).getdId());
+                }
+            }
+        });
+
+        holder.mPraiseCount.setText("" + ((DynamicOneselfBean) mList.get
+                (position).get
+                ("DynamicOneselfBean")).getPraiseCount());
+
+        holder.mCommentImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, DynamicCommentActivity.class);
+                intent.putExtra("myId", (((MyApplication) ((DynamicOneselfActivity) mContext)
+                        .getApplication
+                                ()).getUserInfo().getUid()));
+                DynamicOneselfBean oneselfBean = (DynamicOneselfBean) mList.get(position).get
+                        ("DynamicOneselfBean");
+                DynamicImgBean imgBean = new DynamicImgBean(oneselfBean.getdId(), oneselfBean
+                        .getName(), oneselfBean.getHeadPhoto(), oneselfBean.getImgList(),
+                        oneselfBean.getContent(), oneselfBean.getTime(), oneselfBean
+                        .getLocation(), oneselfBean.getPraiseCount(), oneselfBean
+                        .getCommentCount(), oneselfBean.getuId(), oneselfBean.getPraiseStatus());
+                Log.d("TAG", imgBean.toString());
+                intent.putExtra("dynamicBean", imgBean);
+                mContext.startActivity(intent);
+            }
+        });
+
+        holder.mCommentCount.setText("" + ((DynamicOneselfBean)
+                mList.get(position)
+                        .get("DynamicOneselfBean")).getCommentCount());
+        return convertView;
+    }
+
+    class ViewHolder {
         public TextView mTime, mContent;
         public NineGridView mGridView;
         TextView mPraiseCount, mCommentCount;
         ImageView mPraiseImg, mCommentImg;
 
         public ViewHolder(View itemView) {
-            super(itemView);
             mTime = (TextView) itemView.findViewById(R.id.dynamic_oneself_imgItem_time);
             mContent = (TextView) itemView.findViewById(R.id.dynamic_oneself_imgItem_content);
             mGridView = (NineGridView) itemView.findViewById(R.id.dynamic_oneself_imgItem_gridView);
@@ -257,34 +195,6 @@ public class DynamicOneselfAdapter extends RecyclerView.Adapter<RecyclerView.Vie
             mCommentCount = (TextView) itemView.findViewById(R.id
                     .dynamic_oneself_imgItem_commentCount);
         }
-    }
-
-    class HeaderViewHolder extends RecyclerView.ViewHolder {
-        ImageView mBackImageView;
-        ImageView mHeadImageView;
-        TextView mNameTextView;
-        ImageView mSexImageView;
-
-        public HeaderViewHolder(View itemView) {
-            super(itemView);
-            mBackImageView = (ImageView) itemView.findViewById(R.id.dynamic_header_background);
-            mHeadImageView = (ImageView) itemView.findViewById(R.id.dynamic_header_head_img);
-            mNameTextView = (TextView) itemView.findViewById(R.id.dynamic_header_name);
-            mSexImageView = (ImageView) itemView.findViewById(R.id.personSex);
-        }
-    }
-
-    class FooterViewHolder extends RecyclerView.ViewHolder {
-        TextView mLoadText;
-
-        public FooterViewHolder(View itemView) {
-            super(itemView);
-            mLoadText = (TextView) itemView.findViewById(R.id.dynamic_footer_content);
-        }
-    }
-
-    public interface OnItemClickListener {
-        void onItemClick(int position, HashMap<String, Object> map);
     }
 
     //日期处理
